3996d4cb19d977e091ce93d9cf1f6943496c3cc4
toanlc01 <congtoanle01@gmail.com>
2021-12-17 09:59:08 -0800

    add all files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, session
 import bcrypt, random
+from werkzeug.utils import secure_filename
 import cs304dbi as dbi
 
 app = Flask(__name__)
@@ -9,12 +10,7 @@ app.secret_key = ''.join([random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
                                          '0123456789'))
                           for i in range(20)])
 
-
 @app.route("/")
-def index():
-    return "This is a test message"
-
-@app.route("/home")
 def home():
     if 'logged_in' in session:
         is_logged_in = session['logged_in']
@@ -23,14 +19,27 @@ def home():
         return render_template("landingPage.html")
     return render_template("landingPage.html")
 
-@app.route("/events")
-def events():
+@app.route("/home")
+def homey():
     if 'logged_in' in session:
         is_logged_in = session['logged_in']
         if is_logged_in:
-            return render_template('event.html')
-        return redirect(url_for('home'))
+            return redirect(url_for('events'))
+        return render_template("landingPage.html")
+    return render_template("landingPage.html")    
+
+@app.route("/events")
+def events():
+    if 'email' in session:
+        dbi.conf(db='lect_db')
+        conn = dbi.connect()
+        curs = dbi.cursor(conn)
+        curs.execute("SELECT * FROM Events;")
+        results = curs.fetchall()
+        print(results)
+        return render_template('event.html', events=results)
     return redirect(url_for('home'))
+
 
 @app.route("/my-event")
 def myEvent():
@@ -47,6 +56,34 @@ def myEvent():
         return render_template('myEvent.html', allEvents=allEvents)
     flash("You are not logged in")
     return redirect(url_for('home'))
+
+@app.route("/createEvent", methods=["GET", "POST"])
+def createEvents():
+    if request.method == "GET":
+        return render_template("eventForm.html")
+    else:
+        try:
+            eventName= request.form["eventName"]
+            location= request.form["location"]
+            time= request.form['eventTime']
+            description =request.form['description']
+            if request.files['file'].filename != '':
+                print('hi')
+                filename = secure_filename(request.files['file'].filename)
+                request.files['file'].save(filename)
+            dbi.conf(db='lect_db')
+            con= dbi.connect()
+            curs= dbi.cursor(con)
+            # query2=f" insert into Events values (NULL, '{eventName}', '{description}', '{time}', '{location}', {session['stuid']});"
+            # print(query2)
+            curs.execute("insert into Events(eid, title, descrip, time, location, sid) VALUES (NULL, %s, %s, %s, %s, %s);", [eventName, description, time, location, session['stuid']])
+            # crs.execute(query2)
+            con.commit()
+            flash("Event added")
+            return render_template('eventForm.html')
+        except Exception as err:
+            flash(f"form submission error {err}.\n Please try again!")
+            return render_template('eventForm.html')
 
 @app.route("/log-in", methods=["POST"])
 def login():
